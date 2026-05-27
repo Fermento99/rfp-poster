a7173265e6f240999c248eab22414166b70a1c8f
Paweł Kajanek <pawel.kajanek@stxnext.pl>
2026-05-27 13:50:26 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RFP Poster
+
+A repo for hosting a lineup for Rock For People festival that is suitable for printing.
---
+++ config.js
@@ -0,0 +1,12 @@
+const config = {
+  "stages": {
+    "Mastercard Stage": "large",
+    "Rock for People Stage": "big",
+    "E2 Stage": "big",
+    "Petr Svoboda Stage": "medium",
+    "ČT art Stage": "medium",
+    "Reflex Stage": "small",
+    "EcoFlow Stage": "small",
+    "Karaoke Stage": "small"
+  }
+}
